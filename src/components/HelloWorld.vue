237e91fe0ab0bae50752db98ce1cb9980c9177ce
norinmura <nswe.nrin@gmail.com>
2018-11-09 15:45:51 -0500

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,7 @@
       For guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      or get help from google. 
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
