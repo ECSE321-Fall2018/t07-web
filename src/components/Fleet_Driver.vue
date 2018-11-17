78fa3d4dd35209f8b69db39dc9192248cf991b03
norinmura <nswe.nrin@gmail.com>
2018-11-17 18:45:16 -0500

    Modified layout so it's easier to add stuffs

---
+++ src/components/Fleet_Driver.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="eventregistration">
+    <h2>Participants</h2>
+    <table>
+      <tr>
+          <td>John</td>
+          <td>Event to attend</td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text" placeholder="Participant Name">
+          </td>
+          <td>
+              <button>Create</button>
+          </td>
+      </tr>
+    </table>
+    <p>
+      <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+
+
+<style>
+</style>
