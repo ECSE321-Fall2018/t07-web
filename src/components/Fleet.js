a4455fc13c1dcc5e347386b5576860acf559ed88
norinmura <nswe.nrin@gmail.com>
2018-11-18 02:37:59 -0500

    Active Routes menu configured but not implemented

--- src/components/Fleet.js
+++ src/components/Fleet.js
@@ -22,20 +22,44 @@ export default {
   data () {
     return {
       trips: [],
+      type: 'A',
+      title: 'Overview',
     }
   },
   created: function () {
-		AXIOS.get(`/trips`)
-		.then(response => {
-			// JSON responses are automatically parsed.
-			this.trips = response.data
-		})
-		.catch(e => {
-			this.errorParticipant = e;
-		});
+  		this.type = 'A';
+  		this.title = 'Overview';
+  		this.Overview();
 	}, 
 	methods: {
-
+		Overview: function() {
+			this.title = 'Overview';
+			this.type = 'A';
+			AXIOS.get(`/trips`)
+			.then(response => {
+				// JSON responses are automatically parsed.
+				this.trips = response.data
+			})
+			.catch(e => {
+				this.errorParticipant = e;
+			});
+		},
+		ActiveRoutes: function() {
+			this.title = 'Active Routes';
+			this.type = 'B';
+			
+			var date=new Date(); 
+			var today = date.getFullYear() + "-"+(date.getMonth()+1)+"-"+date.getDate();
+						
+			AXIOS.post(`/trips/search?date=` + today)
+			.then(response => {
+				// JSON responses are automatically parsed.
+				this.trips = response.data
+			})
+			.catch(e => {
+				this.errorParticipant = e;
+			});
+		}
 	}
 }
 
--- src/components/Fleet.vue
+++ src/components/Fleet.vue
@@ -5,25 +5,27 @@
 	        <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
 	          <ul class="nav nav-pills flex-column">
 	            <li class="nav-item">
-	              <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+	              <a class="nav-link" v-bind:class="{ active: type === 'A' }" v-on:click="Overview" href="#">Overview</a>
 	            </li>
 	            <li class="nav-item">
-	              <a class="nav-link" href="/FleetRoutes">By Routes</a>
+	              <a class="nav-link" v-bind:class="{ active: type === 'B' }" v-on:click="ActiveRoutes" href="#">By Routes</a>
 	            </li>
 	            <li class="nav-item">
-	              <a class="nav-link" href="/FleetDrivers">By Drivers</a>
+	              <a class="nav-link" href="#">By Drivers</a>
 	            </li>
 	            <li class="nav-item">
-	              <a class="nav-link" href="/FleetPassengers">By Passengers</a>
+	              <a class="nav-link" href="#">By Passengers</a>
 	            </li>
 	          </ul>
 	        </nav>
 	      </div>
 	    </div>
 	    <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
-	    	<h1>Overview</h1>
-	          <div class="table-responsive">
-	            <table class="table table-striped">
+	    	<h1>{{ title }}</h1>
+	    	
+	    	<!-- For Overview -->
+	          <div class="table-responsive" v-if="type=='A'">
+	            <table class="table table-striped" >
 	              <thead>
 	                <tr>
 	                  <th>Completed</th>
@@ -46,6 +48,39 @@
 	              </tbody>
 	            </table>
 	          </div>
+	          
+	          <!-- For Active Routes -->
+	          <div class="table-responsive" v-else-if="type=='B'">
+	            <p>
+	            	For now, it's showing all active routes for TODAY. <br />
+					We might have to discuss the presentation of each specific options. <br />
+					Could have: Date picker (so provide option to see the data from past). 
+				</p>
+	            <table class="table table-striped" >
+	              <thead>
+	                <tr>
+	                  <th>Active</th>
+	                  <th>Driver</th>
+	                  <th>Date</th>
+	                  <th>Time</th>
+	                  <th>Departure</th>
+	                  <th>Destination</th>
+	                </tr>
+	              </thead>
+	              <tbody>
+	                <tr v-for="trip in trips">
+	                  <td>{{ !trip.isCompleted }}</td>
+	                  <td>{{ trip.firstname }} {{ trip.lastname }}</td>
+	                  <td>{{ trip.departure_date }}</td>
+	                  <td>{{ trip.departure_time }}</td>
+	                  <td>{{ trip.departure_location }}</td>
+	                  <td>{{ trip.destinations }}</td>
+	                </tr>
+	              </tbody>
+	            </table>
+	          </div>
+	          
+	          
 	    </main>
 	</div>
 </template>
