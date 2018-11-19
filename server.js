61e7f5557c70886a7da006d8eb12793f1554894b
norinmura <nswe.nrin@gmail.com>
2018-11-18 19:39:54 -0500

    Heroku setup 1

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 8000;
-app.listen(port);
+var port = process.env.PORT || 8087;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
 console.log('server started '+ port);
+
+
